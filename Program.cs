3a4eafba28fe16690a65eed9e724fff5ecc51891
Epiger <wigo118@hotmail.de>
2018-08-16 13:02:28 +0200

    dotnet core init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jabbar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
