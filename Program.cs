cfd2e786fd0b86c6640f79575c3d6129f4aba84b
Epiger <wigo118@hotmail.de>
2018-08-16 17:17:37 +0200

    startup + enter directory

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
 
-namespace Jabbar
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Jabbar {
+
+    
+    class Program{
+
+        static string jabbarLo = "\r\n\r\n    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\r\n    ░░░░░░░██░████████░████████░████████░░████████░███████░░░\r\n    ░░░░░░░██░██░░░░██░██░░░░██░██░░░░░██░██░░░░██░██░░░░██░░\r\n    ░░░░░░░██░██░░░░██░██░░░░██░██░░░░░██░██░░░░██░██░░░░██░░\r\n    ░░░░░░░██░██░░░░██░██░░░░██░██░░░░░██░██░░░░██░██░░░░██░░\r\n    ░░░░░░░██░████████░████████░████████░░████████░███████░░░\r\n    ░░░░░░░██░██░░░░██░██░░░░██░██░░░░░██░██░░░░██░██░░██░░░░\r\n    ░░░░░░░██░██░░░░██░██░░░░██░██░░░░░██░██░░░░██░██░░░██░░░\r\n    ░░███████░██░░░░██░██░░░░██░████████░░██░░░░██░██░░░░██░░\r\n    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░";
+
+        static void Main(string[] args){
+            if(args.Length == 0){
+                Console.WriteLine(jabbarLo);
+                Console.WriteLine("\r\n         ##############################################\r\n         # Project analyzer for removing unused files #\r\n         ##############################################");
+                Console.WriteLine("\r\n\r\n-------------------------------------------------------------------\r\n");
+            }
+
+            Console.WriteLine("Enter working directory, [Enter] for the current directory");
+            Console.Write($"{Directory.GetCurrentDirectory()}> ");
+            string dir = Console.ReadLine();
+
+            if(dir == "")
+                dir = Directory.GetCurrentDirectory();
+            
+
+            if(!Directory.Exists(dir)){
+                Console.WriteLine("This directory doesn't exist!!");
+                Console.ReadKey();
+                return;
+            }
+
+
+            
+            
+
+            
+
+            Console.ReadKey();
         }
     }
 }
