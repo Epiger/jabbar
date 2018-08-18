986a911a470517b035cd7bd3f772ac1b6188d533
Epiger <wigo118@hotmail.de>
2018-08-18 03:09:41 +0200

    moved HelloWorld.java

---
+++ exJaPro/src/com/none/HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.none.HelloWorld;
+
+import javax.swing.JWindow;
+
+import com.sun.glass.ui.Size;
+
+public class HelloWorld {
+
+    public static main(string[] args){
+        System.out.println("HelloWorld");
+
+        JWindow window = new JWindow();
+        
+        window.setPreferredSize(new Size(300, 400));
+        window.setVisible(true);
+        
+    }
+
+}
