158a416aef65b6488f4a6f68a0a47a2b122a32d6
Epiger <wigo118@hotmail.de>
2018-08-19 02:21:29 +0200

    Added first Java keywords

---
+++ KeyWords.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Jabbar.Keywords {
+
+
+
+
+    public static class Java {
+
+        public static List<string> accessLevelModifiers = new List<string>(){"public", "private", "protected"};
+
+        public static List<string> initialValue = new List<string>(){"static"};
+
+        public static List<string> subclassable = new List<string>(){"abstract", "final"};
+        
+    }
+
+
+}
