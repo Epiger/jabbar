6cd95573a6e34eea8c41bd422ec132598f2cc3d0
Epiger <wigo118@hotmail.de>
2018-08-19 16:04:19 +0200

    Chnaged to one method for all CIEAs (ClassInterfaceEnumAnnotation)

--- JavaFileInterpreter.cs
+++ JavaFileInterpreter.cs
@@ -29,13 +29,14 @@ namespace Jabbar{
                 Console.WriteLine(import);
             }
             //Classes
-            foreach(string clas in GetClasses(path, file)){
+            foreach(string clas in getCIEAsByKeyword("class", path, file)){
                 Console.WriteLine(JavaCIEAHolder.getObject(clas).content);
             }
             //Interfaces
-            foreach(string inter in GetInterfaces(path, file)){
-                Console.WriteLine(JavaCIEAHolder.getObject(inter).content);
+            foreach(string clas in getCIEAsByKeyword("interface", path, file)){
+                Console.WriteLine(JavaCIEAHolder.getObject(clas).content);
             }
+
 
 
 
@@ -62,103 +63,23 @@ namespace Jabbar{
             return imports;
         }
 
-        public List<string> GetClasses(string path, string file){
-            List<string> classes = new List<string>();
+
+        ///<summary>
+        ///CIEA stands for Class Interface Enum and Annotation
+        ///</summary>
+        public List<string> getCIEAsByKeyword(string keyword, string path, string file){
+            List<string> cieas = new List<string>();
             int offset = 0;
-            while(file.IndexOf("class", offset) > -1) {
-                int clas = file.IndexOf("class", offset);
+            while(file.IndexOf(keyword, offset) > -1) {
+                int inter = file.IndexOf(keyword, offset);
                 int start = 0;
                 int end = 0;
 
                 bool foundStart = false;
 
-                foreach(string keyword in Java.accessLevelModifiers){
-                    if(file.IndexOf(keyword, clas - 30, 30) > -1){
-                        start = file.IndexOf(keyword, clas - 30, 30);
-                        foundStart = true;
-                        break;
-                    }                 
-                }
-
-
-                if(file.IndexOf(Java.initialValue[0], clas - 20, 20) > -1 && !foundStart){
-                    start = file.IndexOf(Java.initialValue[0], clas - 20, 20);
-                }
-
-                //If there is no accessLevelModifer and no static it will search for final or abstract
-                foreach(string keyword in Java.subclassable){
-                    if(file.IndexOf(keyword, clas - 12, 12) > -1 && !foundStart){
-                        start = file.IndexOf(keyword, clas - 12, 12);
-                        foundStart = true;
-                        break;
-                    }                 
-                }
-
-                //If there is no private, public, protected, static, final or abstract the begin of the class is set to the class keyword
-                if(!foundStart){
-                    start = clas;
-                }
-
-
-                int openBrackets = 0;
-                int nextBracketOffset = start;
-                do {
-                    int nextOpen = file.IndexOf("{", nextBracketOffset);
-                    int nextClose = file.IndexOf("}", nextBracketOffset);
-                    if(nextOpen == -1){
-                        if(nextClose != -1){
-                            //01
-                            openBrackets--;
-                            nextBracketOffset = file.IndexOf("}", nextBracketOffset)+1;
-                        }else {
-                            //00
-                            throw new NotClosedBracketsException($"Some of the brackets in file: {path}, weren\'t closed!");
-                        }
-                    }else if(nextClose == -1 && nextOpen != -1){
-                        //10
-                        throw new NotClosedBracketsException($"Some of the brackets in file: {path}, weren\'t closed!");
-                    }else if(file.IndexOf("{", nextBracketOffset) < file.IndexOf("}", nextBracketOffset)){
-                        //11
-                        openBrackets++;
-                        nextBracketOffset = file.IndexOf("{", nextBracketOffset)+1;
-                    }else {
-                        //11
-                        openBrackets--;
-                        nextBracketOffset = file.IndexOf("}", nextBracketOffset)+1;
-                    }
-
-                } while (openBrackets != 0);
-
-
-                end = nextBracketOffset;
-                offset = end;
-                //offset = clas + 5;
-
-                //classes.Add(file.Substring(start, end - start));
-                classes.Add(JavaCIEAHolder.getJsonObject(new JavaCIEAHolder(file.Substring(start, end - start), start, end)));
-
-
-
-            }
-
-
-            return classes;
-        }
-
-
-        public List<string> GetInterfaces(string path, string file) {
-            List<string> interfaces = new List<string>();
-            int offset = 0;
-            while(file.IndexOf("interface", offset) > -1) {
-                int inter = file.IndexOf("interface", offset);
-                int start = 0;
-                int end = 0;
-
-                bool foundStart = false;
-
-                foreach(string keyword in Java.accessLevelModifiers){
-                    if(file.IndexOf(keyword, inter - 30, 30) > -1){
-                        start = file.IndexOf(keyword, inter - 30, 30);
+                foreach(string modi in Java.accessLevelModifiers){
+                    if(file.IndexOf(modi, inter - 30, 30) > -1){
+                        start = file.IndexOf(modi, inter - 30, 30);
                         foundStart = true;
                         break;
                     }                 
@@ -170,9 +91,9 @@ namespace Jabbar{
                 }
 
                 //If there is no accessLevelModifer and no static it will search for final or abstract
-                foreach(string keyword in Java.subclassable){
-                    if(file.IndexOf(keyword, inter - 12, 12) > -1 && !foundStart){
-                        start = file.IndexOf(keyword, inter - 12, 12);
+                foreach(string subcl in Java.subclassable){
+                    if(file.IndexOf(subcl, inter - 12, 12) > -1 && !foundStart){
+                        start = file.IndexOf(subcl, inter - 12, 12);
                         foundStart = true;
                         break;
                     }                 
@@ -215,24 +136,20 @@ namespace Jabbar{
 
 
                 end = nextBracketOffset;
-                //offset = end;
-                offset = inter + 5;
+                offset = end;
+                //offset = inter + 5;
 
                 //interfaces.Add(file.Substring(start, end - start));
-                interfaces.Add(JavaCIEAHolder.getJsonObject(new JavaCIEAHolder(file.Substring(start, end - start), start, end)));
+                cieas.Add(JavaCIEAHolder.getJsonObject(new JavaCIEAHolder(file.Substring(start, end - start), start, end)));
 
 
 
             }
 
 
-            return interfaces;
+            return cieas;
+
         }
-
-
-
-
-
 
         
 
