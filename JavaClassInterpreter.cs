8e440e98cdf29974f5ebdb9c0bb0a7e358cf3537
Epiger <wigo118@hotmail.de>
2018-08-22 00:58:15 +0200

    List of all Refs

---
+++ JavaClassInterpreter.cs
@@ -0,0 +1,20 @@
+using System;
+using Jabbar.Graph;
+using System.Collections.Generic;
+
+
+
+namespace Jabbar {
+
+    public class JavaClassInterpreter {
+
+        public List<Node> InterpretClass(string clas){
+
+            
+
+            return null;
+        }
+    }
+
+
+}
